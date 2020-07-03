f18349e3b27f36763156c4c3c91dd1268f4f2137
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2020-07-03 01:34:08 -0400

    Parse config file and serve url list

--- mirrorlist.go
+++ mirrorlist.go
@@ -6,8 +6,18 @@ import (
     "log"
     "net/http"
     "net/url"
+    "os"
     "strings"
+
+    "github.com/go-yaml/yaml"
 )
+
+// mirrorMap is a mapping of release: repo: arch: list of urls
+type mirrorMap map[string]map[string]map[string][]string
+
+type mirrorListHandler struct {
+    mirrors mirrorMap
+}
 
 func getOne(vals url.Values, key string) string {
     v := vals[key]
@@ -17,7 +27,14 @@ func getOne(vals url.Values, key string) string {
     return v[0]
 }
 
-func handleRoot(w http.ResponseWriter, req *http.Request) {
+func (h *mirrorListHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+    log.Printf("%s %s %v\n", req.RemoteAddr, req.Method, req.URL)
+
+    if req.Method != "GET" {
+        http.Error(w, "bad method", http.StatusBadRequest)
+        return
+    }
+
     // The "/" pattern matches everything, so we need to check
     // that we're at the root here.
     if req.URL.Path != "/" {
@@ -25,12 +42,7 @@ func handleRoot(w http.ResponseWriter, req *http.Request) {
         return
     }
 
-    if req.Method != "GET" {
-        http.Error(w, "bad method", http.StatusBadRequest)
-        return
-    }
-
-    //log.Printf("%s %v\n", req.Method, req.URL)
+    // Handle the query string
     vals := req.URL.Query()
 
     arch := getOne(vals, "arch")
@@ -51,12 +63,53 @@ func handleRoot(w http.ResponseWriter, req *http.Request) {
         return
     }
 
-    log.Printf("  arch=%q repo=%q release=%q\n", arch, repo, release)
-    fmt.Fprintln(w, "http://sjc.edge.kernel.org/centos/6.10/os/x86_64/")
+    // Look up url list
+    urls, ok := h.mirrors[release][repo][arch]
+    if !ok {
+        http.Error(w, "Invalid release/repo/arch combination", http.StatusNotFound)
+        return
+    }
+
+    for _, url := range urls {
+        fmt.Fprintln(w, url)
+    }
+}
+
+
+func loadConfig(path string) (mirrorMap, error) {
+    f, err := os.Open(path)
+    if err != nil {
+        return nil, fmt.Errorf("Error opening config: %w", err)
+    }
+
+    d := yaml.NewDecoder(f)
+
+    v := make(mirrorMap)
+    err = d.Decode(v)
+    if err != nil {
+        return nil, fmt.Errorf("Error reading config: %w", err)
+    }
+
+    return v, nil
 }
 
 func main() {
-    http.HandleFunc("/", handleRoot)
+    var err error
+    var handler mirrorListHandler
+
+    if len(os.Args) != 2 {
+        fmt.Fprintf(os.Stderr, "Usage: mirrorlist configfile\n")
+        os.Exit(1)
+    }
+    configPath := os.Args[1]
+
+    handler.mirrors, err = loadConfig(configPath)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "%v\n", err)
+        os.Exit(1)
+    }
+
+    http.Handle("/", &handler)
 
     addr := ":8080"
     log.Println("Serving on " + addr)
